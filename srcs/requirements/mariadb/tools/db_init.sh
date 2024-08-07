042f7be4fe2918d9021ac0b9c79d8754e3d7ffc6
dongyeuk <yupab3@naver.com>
2024-08-07 19:54:52 +0900

    1

--- srcs/requirements/mariadb/tools/db_init.sh
+++ srcs/requirements/mariadb/tools/db_init.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 set -e
 
-# 초기화
-# if [ -d "/var/lib/mysql/mysql" ]; then
-#     echo 'MariaDB 데이터 디렉토리가 이미 초기화되었습니다.'
-# else
-    echo 'MariaDB 데이터 디렉토리 초기화...'
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql --skip-test-db
-    echo '초기화 완료.'
+#초기화
+echo 'MariaDB 데이터 디렉토리 초기화...'
+mysql_install_db --user=mysql --datadir=/var/lib/mysql --skip-test-db
+echo '초기화 완료.'
 
-    mysqld --user=mysql --datadir=/var/lib/mysql --bootstrap << EOSQL
-        FLUSH PRIVILEGES;
-        DELETE FROM mysql.user WHERE user='';
-        CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-        CREATE DATABASE ${WORDPRESS_DB_NAME};
-        GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-        FLUSH PRIVILEGES;
+mysqld --user=mysql --datadir=/var/lib/mysql --bootstrap << EOSQL
+    FLUSH PRIVILEGES;
+    DELETE FROM mysql.user WHERE user='';
+    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    CREATE DATABASE IF NOT EXISTS ${WORDPRESS_DB_NAME};
+    GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    FLUSH PRIVILEGES;
 EOSQL
-    echo '초기 설정 완료.'
-# fi
+echo '초기 설정 완료.'
+
+if [ -z "omg" ]; then
+    adduser --disabled-password omg
+fi
+
+chown -R omg:omg /var/lib/mysql
 
 exec "$@"
--- srcs/requirements/wordpress/tools/setup-wordpress.sh
+++ srcs/requirements/wordpress/tools/setup-wordpress.sh
@@ -3,17 +3,24 @@ set -e
 
 cd /var/www/html
 
-wp core download --path=/var/www/html --allow-root
+if wp core download is-installed --path=/var/www/html --allow-root; then
+else
+    wp config create --dbname=${WORDPRESS_DB_NAME} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST} --allow-root
 
-wp config create --dbname=${WORDPRESS_DB_NAME} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST} --allow-root
+    wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
+    wp user create "$WP_GENERAL_USER" "$WP_GENERAL_EMAIL" --role=subscriber --user_pass="$WP_GENERAL_PASSWORD" --allow-root
 
-wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
-wp user create "$WP_GENERAL_USER" "$WP_GENERAL_EMAIL" --role=subscriber --user_pass="$WP_GENERAL_PASSWORD" --allow-root
+    wp option update blogdescription "hiiiiiiiii" --allow-root
+    wp option update timezone_string "Korea/Gaepo" --allow-root
+    wp option update date_format "Y-m-d" --allow-root
+    wp option update time_format "H:i" --allow-root
+    wp option update permalink_structure "/%postname%/" --allow-root
 
-wp option update blogdescription "hiiiiiiiii" --allow-root
-wp option update timezone_string "Korea/Gaepo" --allow-root
-wp option update date_format "Y-m-d" --allow-root
-wp option update time_format "H:i" --allow-root
-wp option update permalink_structure "/%postname%/" --allow-root
+    # if [ -z "omg" ]; then
+    adduser --disabled-password omg
+    # fi
+
+    chown -R omg:omg /var/www/html
+fi
 
 exec php-fpm82 -F

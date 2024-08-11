0e4bb063504845272da579ef867a8be5886749b2
dongyeuk <yupab3@naver.com>
2024-08-11 15:12:37 +0900

    1

--- srcs/requirements/wordpress/tools/setup-wordpress.sh
+++ srcs/requirements/wordpress/tools/setup-wordpress.sh
@@ -12,9 +12,7 @@ else
     wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
     wp user create "$WP_GENERAL_USER" "$WP_GENERAL_EMAIL" --role=subscriber --user_pass="$WP_GENERAL_PASSWORD" --allow-root
 
-    # if [ -z "wordpress" ]; then
     adduser --disabled-password wordpress
-    # fi
 
     chown -R wordpress:wordpress /var/www/html
     chmod -R 777 /var/www/html

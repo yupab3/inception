ad6f6f4c0f63e6c241659bea263da1f23546a6ea
dongyeuk <yupab3@naver.com>
2024-08-11 16:47:16 +0900

    1

--- srcs/requirements/mariadb/tools/db_init.sh
+++ srcs/requirements/mariadb/tools/db_init.sh
@@ -2,24 +2,27 @@
 set -e
 
 #초기화
-echo 'MariaDB 데이터 디렉토리 초기화...'
-mysql_install_db --user=mysql --datadir=/var/lib/mysql --skip-test-db
-echo '초기화 완료.'
+if [ -d "/var/lib/mysql/mysql" ]; then
+    echo 'MariaDB 가 이미 초기화 되어있습니다.'
+else
+    echo 'MariaDB 데이터 디렉토리 초기화...'
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql --skip-test-db
+    echo '초기화 완료.'
 
-mysqld --user=mysql --datadir=/var/lib/mysql --bootstrap << EOSQL
-    FLUSH PRIVILEGES;
-    DELETE FROM mysql.user WHERE user='';
-    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-    CREATE DATABASE IF NOT EXISTS ${WORDPRESS_DB_NAME};
-    GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    mysqld --user=mysql --datadir=/var/lib/mysql --bootstrap << EOSQL
+        FLUSH PRIVILEGES;
+        DELETE FROM mysql.user WHERE user='';
+        CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+        CREATE DATABASE IF NOT EXISTS ${WORDPRESS_DB_NAME};
+        GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 EOSQL
-echo '초기 설정 완료.'
+    echo '초기 설정 완료.'
 
-if [ -z $(getent passwd "omg") ]; then
+    if [ -z $(getent passwd "omg") ]; then
     adduser --disabled-password omg
+    chown -R omg:omg /var/lib/mysql
+    chmod -R 777 /var/lib/mysql
+    fi
 fi
-
-chown -R omg:omg /var/lib/mysql
-chmod -R 777 /var/lib/mysql
 
 exec "$@"
